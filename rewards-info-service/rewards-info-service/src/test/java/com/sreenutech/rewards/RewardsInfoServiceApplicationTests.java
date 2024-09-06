1efc6cb8f480f949321b1978d033b2c2c5d8e23d
PremGit <premoruganti91@gmail.com>
2024-09-06 21:03:48 +0530

    skelton added

---
+++ rewards-info-service/rewards-info-service/src/test/java/com/sreenutech/rewards/RewardsInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreenutech.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
